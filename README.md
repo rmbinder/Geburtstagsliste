2151f9bee65339c6d85ee94ca21f1be6dc0cab69
rmbinder <r-m-b@gmx.net>
2017-12-03 20:43:32 +0100

    link to documentation updated , #16

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 Geburtstagsliste erzeugt für einen bestimmten Zeitraum eine Geburtstags- und Jubiläumsliste der Mitglieder.
 
-Geburtstagsliste ist ein Plugin zu der Online-Mitgliederverwaltung Admidio (http://www.admidio.org) und kann nicht ohne diese benutzt werden.
+Geburtstagsliste ist ein Plugin zu der Online-Mitgliederverwaltung Admidio (https://www.admidio.org) und kann nicht ohne diese benutzt werden.
 
-Eine Online-Dokumentation gibt es hier: http://www.admidio.de/dokuwiki/doku.php?id=de:plugins:geburtstagsliste#geburtstagsliste
+Eine Online-Dokumentation gibt es hier: https://www.admidio.org/dokuwiki/doku.php?id=de:plugins:geburtstagsliste#geburtstagsliste
 
 ## Änderungshistorie
 
-In der [Änderungshistorie](http://www.admidio.de/dokuwiki/doku.php?id=de:plugins:geburtstagsliste#aenderungshistorie) gibt es detaillierte Informationen über die Fehlerbehebungen und Verbesserungen in jeder Version.
+In der [Änderungshistorie](https://www.admidio.org/dokuwiki/doku.php?id=de:plugins:geburtstagsliste#aenderungshistorie) gibt es detaillierte Informationen über die Fehlerbehebungen und Verbesserungen in jeder Version.
--- preferences.php
+++ preferences.php
@@ -467,7 +467,7 @@ $page->addHtml('
                         $form->addStaticControl('plg_version', $gL10n->get('PLG_GEBURTSTAGSLISTE_PLUGIN_VERSION'), $pPreferences->config['Plugininformationen']['version']);
                         $form->addStaticControl('plg_date', $gL10n->get('PLG_GEBURTSTAGSLISTE_PLUGIN_DATE'), $pPreferences->config['Plugininformationen']['stand']);
                         
-                        $html = '<a class="icon-text-link" href="http://www.admidio.de/dokuwiki/doku.php?id=de:plugins:geburtstagsliste" target="_blank"><img
+                        $html = '<a class="icon-text-link" href="https://www.admidio.org/dokuwiki/doku.php?id=de:plugins:geburtstagsliste#geburtstagsliste" target="_blank"><img
                                     src="'. THEME_URL . '/icons/eye.png" alt="'.$gL10n->get('PLG_GEBURTSTAGSLISTE_DOCUMENTATION_OPEN').'" />'.$gL10n->get('PLG_GEBURTSTAGSLISTE_DOCUMENTATION_OPEN').'</a>';
                         $form->addCustomContent($gL10n->get('PLG_GEBURTSTAGSLISTE_DOCUMENTATION'), $html, array('helpTextIdInline' => 'PLG_GEBURTSTAGSLISTE_DOCUMENTATION_OPEN_DESC'));
                         $page->addHtml($form->show(false));
