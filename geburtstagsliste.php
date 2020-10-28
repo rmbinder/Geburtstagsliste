7110e57f0cc3e93ad289eca3aff045e7018241ea
rmbinder <r-m-b@gmx.net>
2020-10-28 20:35:49 +0100

    update version

--- geburtstagsliste.php
+++ geburtstagsliste.php
@@ -3,7 +3,7 @@
  ***********************************************************************************************
  * Geburtstagsliste
  *
- * Version 3.0.0-Beta1
+ * Version 3.0.0
  *
  * Dieses Plugin erzeugt fuer einen bestimmten Zeitraum eine Geburtstags- und Jubilaeumsliste der Mitglieder.
  *
