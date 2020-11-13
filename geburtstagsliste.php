04bfb48100f7963723a41e5176bfe3a75cb262e6
rmbinder <r-m-b@gmx.net>
2020-11-13 19:57:38 +0100

    change lists_members_per_page to groups_roles_members_per_page

--- geburtstagsliste.php
+++ geburtstagsliste.php
@@ -401,7 +401,7 @@ if ($getMode != 'csv')
         $page->addHtml($form->show());
         
         $table = new HtmlTable('adm_lists_table', $page, $hoverRows, $datatable, $classTable);
-        $table->setDatatablesRowsPerPage($gSettingsManager->getInt('lists_members_per_page'));
+        $table->setDatatablesRowsPerPage($gSettingsManager->getInt('groups_roles_members_per_page'));
     }
 	else
 	{
