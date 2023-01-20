9c95287ccd251e456012e779c1602b39177b91d9
rmbinder <r-m-b@gmx.net>
2023-01-20 20:24:35 +0100

    new year 2023

--- version.php
+++ version.php
@@ -3,11 +3,11 @@
  ***********************************************************************************************
  * Versionsdatei fuer das Admidio-Plugin Geburtstagsliste
  *
- * @copyright 2004-2022 The Admidio Team
+ * @copyright 2004-2023 The Admidio Team
  * @see https://www.admidio.org/
  * @license https://www.gnu.org/licenses/gpl-2.0.html GNU General Public License v2.0 only
  ***********************************************************************************************
  */
 
-$plugin_version = '3.2.0';
-$plugin_stand 	= '05.01.2022';
+$plugin_version = '3.3.0-Beta1';
+$plugin_stand 	= '20.01.2023';
